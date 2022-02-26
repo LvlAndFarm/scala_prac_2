49da66a42c314e087e990a1e082292dad7b401c5
Walid Sabihi <walid.sabihi@cs.ox.ac.uk>
2022-02-26 14:20:48 +0000

    Initial code

---
+++ IntSet.scala
@@ -0,0 +1,98 @@
+// A class of objects to represent a set
+
+class IntSet{
+  // State: S : P(Int) (where "P" represents power set)
+
+  // The following lines just define some aliases, so we can subsequently
+  // write "Node" rather than "IntSet.Node".
+  private type Node = IntSet.Node
+  // Constructor
+  private def Node(datum: Int, next: Node) = new IntSet.Node(datum, next)
+
+  // Init: S = {}
+  private var theSet : Node = Node(0, null) // or however empty set is represented
+  private var _size = 0
+
+  /** Convert the set to a string.
+    * (The given implementation is not sufficient.) */
+  override def toString : String = theSet.toString
+
+  /** Add element e to the set
+    * Post: S = S_0 U {e} */
+  def add(e: Int) : Unit = {
+      theSet.next = Node(e, theSet.next)
+  }
+
+  /** Length of the list
+    * Post: S = S_0 && returns #S */
+  def size : Int = {
+      _size
+  }
+
+  /** Does the set contain e?
+    * Post: S = S_0 && returns (e in S) */
+  def contains(e: Int) : Boolean = {
+      var node = theSet.next
+      while (node.next!=null && node.datum != e) {
+          node = node.next
+      }
+      return node != null
+  }
+
+  /** Return any member of the set.  (This is comparable to the operation
+    * "head" on scala.collection.mutable.Set, but we'll use a name that does
+    * not suggest a particular order.)
+    * Pre: S != {}
+    * Post: S = S_0 && returns e s.t. e in S */
+  def any : Int = theSet.next.datum
+
+  /** Does this equal that?
+    * Post: S = S_0 && returns that.S = S */
+  override def equals(that: Any) : Boolean = that match {
+    case s: IntSet => ???
+    case _ => false
+  }
+
+  /** Remove e from the set; result says whether e was in the set initially
+    * Post S = S_0 - {e} && returns (e in S_0) */
+  def remove(e: Int) : Boolean = ???
+    
+  /** Test whether this is a subset of that.
+    * Post S = S_0 && returns S subset-of that.S */
+  def subsetOf(that: IntSet) : Boolean = ???
+
+  // ----- optional parts below here -----
+
+  /** return union of this and that.  
+    * Post: S = S_0 && returns res s.t. res.S = this U that.S */
+  def union(that: IntSet) : IntSet = ???
+
+  /** return intersection of this and that.  
+    * Post: S = S_0 && returns res s.t. res.S = this intersect that.S */
+  def intersect(that: IntSet) : IntSet = ???
+
+  /** map
+    * Post: S = S_0 && returns res s.t. res.S = {f(x) | x <- S} */
+  def map(f: Int => Int) : IntSet = ???
+
+  /** filter
+    * Post: S = S_0 && returns res s.t. res.S = {x | x <- S && p(x)} */
+  def filter(p : Int => Boolean) : IntSet = ???
+}
+
+
+// The companion object
+object IntSet{
+  /** The type of nodes defined in the linked list */
+  private class Node(var datum: Int, var next: Node)
+
+  /** Factory method for sets.
+    * This will allow us to write, for example, IntSet(3,5,1) to
+    * create a new set containing 3, 5, 1 -- once we have defined 
+    * the main constructor and the add operation. 
+    * post: returns res s.t. res.S = {x1, x2,...,xn}
+    *       where xs = [x1, x2,...,xn ] */
+  def apply(xs: Int*) : IntSet = {
+    val s = new IntSet; for(x <- xs) s.add(x); s
+  }
+}
